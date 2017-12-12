65c89985cf2e20d74c5cbaa917352094f0609ea0
jovanxua <jga.binhimedfi@gmail.com>
2017-12-12 08:29:09 +0800

    redux for workshift

--- src/containers/CompanyPolicies/data/workshift/actions.js
+++ src/containers/CompanyPolicies/data/workshift/actions.js
@@ -1,3 +1,20 @@
+/* import * as api from './api';
+import * as actionTypes from './actionTypes';
+
+const update = items => ({
+	type: actionTypes.UPDATE,
+	items,
+});
+
+export const empty = () => ({
+	type: actionTypes.EMPTY,
+});
+
+export const get = payload =>
+	dispatch =>
+		api.get(payload)
+		.then(response => dispatch(update(response.users))); */
+
 import * as api from './api';
 import * as actionTypes from './actionTypes';
 
