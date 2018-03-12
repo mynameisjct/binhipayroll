2e7125ffd0a7ab69afb67b60daa5c1b9fec85571
jovanxua <jga.binhimedfi@gmail.com>
2018-03-12 13:28:03 +0800

    added wireframed payslip

--- src/containers/Reports/payslip/payslipform.js
+++ src/containers/Reports/payslip/payslipform.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    Button
+    Button,
+    ScrollView
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -14,9 +15,10 @@ const oTempData = {
     info: [
         [
             ['ID Number', '2017-1804E'],
-            ['Employee Name', 'Auxilio, Jovanni G.'],
-            ['Position', 'Internal Janitor'],
-            ['systemnewcolumn'],
+            ['Employee Name', 'Auxilio, Jovanni G. '],
+            ['Position', 'Internal Janitor']
+        ],
+        [
             ['Payroll Period', '09/11/2017 - 09/25/2017'],
             ['Pay Date', '09/11/30'],
             ['Basic Salary', '13,000.00 (Monthly)']
@@ -62,7 +64,7 @@ const oTempData = {
                 ['Medecine Allowance', '500'],
                 ['systemdiv'],
                 ['Loans', 'Amount'],
-                ['systemdiv'],
+                ['systembreak'],
                 ['In-House', '500'],
                 ['SSS', '500'],
                 ['MPL (Pag-ibig)', '500'],
@@ -89,13 +91,45 @@ export default class EmployeePayslipForm extends Component {
         const contentStyles = styles.content;
         const titleStyles = styles.title;
         const bodyStyles = styles.body;
+        const systemStyles = styles.system;
+        const textStyles = styles.textStyles;
         return(
             <View style={styles.container}>
                 <View style={headerStyles.container}>
                     <View style={headerStyles.left}>
+                        <View style={headerStyles.iconCont}>
+                            <Icon 
+                            size={40} 
+                            name='md-home' 
+                            color='#434646'/>
+                        </View>
+                        <View style={headerStyles.generalInfoCont}>
+                            <View style={headerStyles.titleCont}>
+                                <Text style={textStyles.companyName}>JCA Realty Corporation</Text>
+                                <Text style={textStyles.address}>#80 Yacapin Sts., Cagayan de Oro City, Misamis Oriental 9000</Text>
+                            </View>
+                        </View>
                     </View>
 
                     <View style={headerStyles.right}>
+                        {
+                            oTempData.info.map((aParamsList, indexParamsList) => 
+                                <View key={indexParamsList} style={headerStyles.paramsList}>
+                                    {
+                                        aParamsList.map((aParamsArgs, indexParamsArgs) => 
+                                            <View key={indexParamsArgs} style={headerStyles.param}>
+                                                <View style={headerStyles.label}>
+                                                    <Text style={textStyles.label}>{aParamsArgs[0]}</Text>
+                                                </View>
+                                                <View style={headerStyles.value}>
+                                                    <Text style={textStyles.value}>{aParamsArgs[1]}</Text>
+                                                </View>
+                                            </View>
+                                        )
+                                    }
+                                </View>
+                            )
+                        }
                     </View>
                 </View>
 
@@ -107,24 +141,70 @@ export default class EmployeePayslipForm extends Component {
                                     <Text>{oData.title}</Text>
                                 </View>
                                 <View style={bodyStyles.container}>
+                                    <ScrollView>
+                                        <View style={bodyStyles.paramsList}>
+                                            {
+                                                oData.list.map((aList, indexList) => {
+                                                    switch(aList[0].toLowerCase()){
+                                                        case 'systembreak':
+                                                            return <View key={indexList} style={systemStyles.break}/>;
+                                                            break;
+                                                        case 'systemdiv':
+                                                            return <View key={indexList} style={systemStyles.div}/>;
+                                                            break;
+                                                        case 'systemfooter':
+                                                            return null;
+                                                            break;
+                                                        default:
+                                                            return(
+                                                                    <View key={indexData} style={bodyStyles.params}>
+                                                                        {
+                                                                            aList.map((strParam, indexParams) =>
+                                                                                <View 
+                                                                                    key={indexParams} 
+                                                                                    style={[bodyStyles.paramsArg, 
+                                                                                        indexParams==0 ? bodyStyles.paramsLeftMost :
+                                                                                        indexParams==(aList.length-1) ? bodyStyles.paramsRightMost :
+                                                                                        bodyStyles.paramsCenter]
+                                                                                    }
+                                                                                >
+
+                                                                                    <Text>{strParam}</Text>
+                                                                                </View>
+                                                                            )
+                                                                        }
+                                                                    </View>
+                                                            );
+                                                    }
+                                                })
+                                            }
+                                            {}
+                                        </View>
+                                    </ScrollView>
                                     {
                                         oData.list.map((aList, indexList) => {
-                                            switch(aList[0].toLowerCase()){
-                                                case 'systembreak':
-                                                    return null;
-                                                    break;
-                                                default:
-                                                    return(
-                                                        <View key={indexList} style={bodyStyles.params}>
-                                                            {
-                                                                aList.map((strParam, indexParams) =>
-                                                                    <View key={indexParams} style={bodyStyles.paramsVal}>
-                                                                        <Text>{strParam}</Text>
-                                                                    </View>
-                                                                )
-                                                            }
+                                            if(aList[0].toLowerCase() === 'systemfooter'){
+                                                return(
+                                                    <View key={indexList} 
+                                                        style={systemStyles.footer.container}>
+
+                                                        <View style={systemStyles.footer.title}>
+                                                            <Text>
+                                                                { aList[1] }
+                                                            </Text>
                                                         </View>
-                                                    );
+
+                                                        <View style={systemStyles.footer.value}>
+                                                            <Text>
+                                                                { aList[2] }
+                                                            </Text>
+                                                        </View>
+                                                        
+                                                    </View>
+                                                )
+                                            }
+                                            else{
+                                                return null;
                                             }
                                         })
                                     }
--- src/containers/Reports/payslip/styles.js
+++ src/containers/Reports/payslip/styles.js
@@ -21,19 +21,69 @@ export default {
 
     header: {
         container: {
-            padding: 10,
             backgroundColor: 'orange',
             flexDirection: 'row'
         },
 
         left: {
+            maxWidth: '25%',
+            minWidth: '25%',
             backgroundColor:'blue',
-            padding: 10
+            flexDirection: 'row',
         },
 
         right: {
+            width: '75%',
             backgroundColor:'indigo',
-            padding: 10
+            flexDirection: 'row',
+            justifyContent: 'center'
+        },
+
+        iconCont: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderWidth: 1,
+            padding: 5
+        },
+
+        generalInfoCont: {
+            maxWidth: '80%',
+            minWidth: '80%',
+            flexDirection: 'column',
+            backgroundColor: 'green',
+            justifyContent: 'center'
+        },
+
+        titleCont: {
+            backgroundColor: 'red'
+        },
+        
+        addressCont: {
+        },
+
+        paramsList: {
+            flexDirection: 'column',
+            borderWidth: 1,
+            width: '50%'
+        },
+
+        param: {
+            flexDirection: 'row',
+            backgroundColor: 'orange',
+            paddingTop: 5
+        },
+
+        label: {
+            width: '40%',
+            borderWidth: 1,
+            paddingLeft: 10,
+            paddingBottom: 5
+        },
+
+        value: {
+            width: '60%',
+            borderWidth: 1,
+            paddingBottom: 5
         }
         
     },
@@ -50,21 +100,111 @@ export default {
     body: {
         container: {
             flex: 1,
-            padding: 10,
             backgroundColor: 'green',
             flexDirection: 'column'
         },
 
-        params: {
+        paramsList: {
             flex: 1,
-            flexDirection: 'row'
+            padding: 7
         },
 
-        paramsVal: {
-            flex: 1
-        }
+        params: {
+            /* flex: 1, */
+            paddingBottom: 7,
+            flexDirection: 'row',
+            borderWidth: 1,
+            borderColor: '#FFF'
+        },
 
+        paramsArg: {
+            flex: 1,
+            borderWidth: 1,
+            justifyContent: 'center',
+            borderColor: 'blue'
+        },
+
+        paramsLeftMost: {
+            flex: 2,
+            alignItems: 'flex-start'
+        },
+
+        paramsRightMost: {
+            flex: 1.2,
+            alignItems: 'flex-end'
+        },
+
+        paramsCenter: {
+            alignItems: 'center'
+        }
     },
 
+    system: {
+        break: {
+            height: 15,
+            backgroundColor: 'blue'
+        },
 
+        div: {
+            height: 1.5,
+            marginTop: 10,
+            marginBottom: 10,
+            backgroundColor: '#838383'
+        },
+
+        footer: {
+            container:{
+                position: 'absolute',
+                backgroundColor:'red',
+                height: 120,
+                right: 0,
+                left: 0,
+                bottom: 0,
+                flexDirection: 'column'
+            },
+            title: {
+                flex: 0.7,
+                backgroundColor: 'yellow',
+                borderTopWidth: 1,
+                borderBottomWidth: 1,
+                borderColor: '#838383',
+                justifyContent: 'center',
+                alignItems: 'center'
+            
+            },
+            value: {
+                flex: 1,
+                backgroundColor: 'orange',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }
+        }
+    },
+
+    textStyles: {
+        companyName: {
+            fontSize: 14,
+            fontFamily: 'Helvetica-Bold',
+            color: '#434646',
+            fontWeight: '500'
+        },
+        
+        address: {
+            fontSize: 11,
+            fontFamily: 'Helvetica-Light',
+            color: '#838383'
+        },
+
+        label: {
+            fontSize: 13,
+            fontFamily: 'Helvetica-Light',
+            color: '#838383'
+        },
+
+        value: {
+            fontSize: 13,
+            fontFamily: 'Helvetica-Light',
+            color: '#434646'
+        }
+    }
 }
