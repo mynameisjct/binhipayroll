5b5c6870f585a99835fbee25f91a1df71a71f888
jovanxua <jga.binhimedfi@gmail.com>
2018-05-10 15:56:53 +0800

    payroll integration

--- src/services/session/selectors.js
+++ src/services/session/selectors.js
@@ -2,3 +2,17 @@ import {store} from '../../store';
 
 export const get = () => store.getState().services.session;
 /* export const get = () => console.log('state: ' + JSON.stringify(store.getState())); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
